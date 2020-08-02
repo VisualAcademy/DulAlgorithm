4c6a7c195c78dac82f548564ecf70197959581f2
RedPlus <redplus@redplus.net>
2020-08-03 04:15:08 +0900

    52장. 클래스 라이브러리와 닷넷 스탠다드 강의 데모 소스

---
+++ DulAlgorithm.WindowsFormsApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DulAlgorithm.WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //[1] Input
+            int[] data = { 1, 4, 3, 2, 5 };
+
+            //[2] Process
+            int[] numbers = DulAlgorithm.Algorithm.SelectionSort(data);
+
+            //[3] Output
+            MessageBox.Show(data[1].ToString()); // 2
+        }
+    }
+}

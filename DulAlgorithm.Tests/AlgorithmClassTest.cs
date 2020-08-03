ec8d334e7c48a5735e00457b6df9ec77a175e4dd
RedPlus <redplus@redplus.net>
2020-08-04 04:38:21 +0900

    테스트 프로젝트 생성 및 테스트 코드 작성

---
+++ DulAlgorithm.Tests/AlgorithmClassTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DulAlgorithm.Tests
+{
+    [TestClass]
+    public class AlgorithmClassTest
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Assert.AreEqual(10, 10);
+        }
+
+        [TestMethod]
+        public void SelectionSort_ShouldReturnSortedArray()
+        {
+            //[1] Arrange(정렬), Setup
+            int[] arr = { 33, 22, 44 };
+
+            //[2] Act(동작), Execute
+            int[] results = DulAlgorithm.Algorithm.SelectionSort(arr);
+
+            //[3] Assert(어설션), Verify
+            Assert.AreEqual(22, results[0]); // true
+            Assert.AreEqual(44, results[results.Length - 1]); // true
+        }
+    }
+}
